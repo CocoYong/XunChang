e59a782470388b9b9104fd3b667ec2d3935fc6fa
zhangyong <zhangyong@ubolixin.com>
2016-05-11 18:15:26 +0800

    addfiles

---
+++ XunChangApp/设置/巡场/XunChangViewController.h
@@ -0,0 +1,13 @@
+//
+//  XunChangViewController.h
+//  XunChangApp
+//
+//  Created by MrZhang on 16/5/9.
+//  Copyright © 2016年 zhangyong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface XunChangViewController : BaseViewController
+
+@end
