e204f660851e45b2978b076cf7cf8e37ff2f4557
zhangyong <zhangyong@ubolixin.com>
2016-05-19 17:37:03 +0800

    commit

---
+++ XunChangApp/消息/Controller/MessagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagerViewController.h
+//  XunChangApp
+//
+//  Created by MrZhang on 16/5/12.
+//  Copyright © 2016年 zhangyong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MessagerViewController : BaseViewController
+
+@end
