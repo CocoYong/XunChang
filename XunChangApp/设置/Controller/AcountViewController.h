c9696ac6806489c068d110b54e14faa16cc6511c
zhangyong <zhangyong@ubolixin.com>
2016-05-25 22:22:51 +0800

    add

---
+++ XunChangApp/设置/Controller/AcountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AcountViewController.h
+//  XunChangApp
+//
+//  Created by MrZhang on 16/5/25.
+//  Copyright © 2016年 zhangyong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AcountViewController : BaseViewController
+
+@end
---
+++ XunChangApp/设置/View/SettingTableCell.h
@@ -0,0 +1,16 @@
+//
+//  SettingTableCell.h
+//  XunChangApp
+//
+//  Created by MrZhang on 16/5/25.
+//  Copyright © 2016年 zhangyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableCell : UITableViewCell
+//settingTableCell
+@property (weak, nonatomic) IBOutlet UILabel *mainTitleLael;
+@property (weak, nonatomic) IBOutlet UIButton *detailButt;
+
+@end
