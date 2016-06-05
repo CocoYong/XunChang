5a1520d89160678955d87c1de5f922078ea6eeb1
MrZhang <zhyo0911@aliyun.com>
2016-06-05 20:37:35 +0800

    change

--- XunChangApp/CommonFuncs.h
+++ XunChangApp/CommonFuncs.h
@@ -21,13 +21,16 @@
 
 #define PHPNOTICEURL @""
 
-//test
+//test zhongming
+//#define BASICURL @"http://kcwc.zhongming.i.ubolixin.com/"
+//#define IMAGEUPLOAD @"http://kcwc.zhongming.i.ubolixin.com/api/upload/index"
+//test luofei
 //#define BASICURL @"http://kcwc.luofei.i.ubolixin.com/"
 //#define IMAGEUPLOAD @"http://kcwc.luofei.i.ubolixin.com/api/upload/index"
-
 //接口地址
 #define BASICURL @"http://www.51kcwc.com/"
 #define IMAGEUPLOAD @"http://www.51kcwc.com/api/upload/index"
+
 #define EVALUATEORDER @"api/xcapply/comment"
 #define USERCENTER @"api/xcapply/usercenter"
 #define GETMOBILEVERIFY @"api/main/getMobileVerify"
